5d40c4ea73a2638187acd50d8553d88dc7eba381
minyk <minykreva@gmail.com>
2017-12-20 21:55:07 +0900

    Corvus is too old. So remove it.

--- frameworks/redis/src/main/java/com/mesosphere/sdk/redis/api/RedisMonitorResource.java
+++ frameworks/redis/src/main/java/com/mesosphere/sdk/redis/api/RedisMonitorResource.java
@@ -1,16 +1,16 @@
 package com.mesosphere.sdk.redis.api;
 
-import com.mesosphere.sdk.api.ResponseUtils;
+//import com.mesosphere.sdk.api.ResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
+//import redis.clients.jedis.Jedis;
 //import redis.clients.jedis.JedisMonitor;
 
-import javax.ws.rs.GET;
+//import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
 //import java.util.HashMap;
 //import java.util.Map;
 
@@ -22,35 +22,11 @@ import javax.ws.rs.core.Response;
 public class RedisMonitorResource {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final String VIP_POST_FIX = ".l4lb.thisdcos.directory";
-    private static final int VIP_SERVICE_PORT = 6379;
-    private Jedis proxyClient;
     private String proxyHost;
 
     public RedisMonitorResource(String name) {
         this.proxyHost = "proxy." + name + VIP_POST_FIX;
         logger.debug("Monitor redis at " + proxyHost);
-    }
-
-    @GET
-    @Path("/proxy")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response getProxyInfo() {
-        if (getProxyClient().isConnected()) {
-            return ResponseUtils.plainOkResponse(proxyClient.info());
-        } else {
-            return Response.serverError().build();
-        }
-
-    }
-
-
-    private Jedis getProxyClient() {
-        if (proxyClient == null) {
-            logger.info("Connecting redis proxy: " + proxyHost + ":" + VIP_SERVICE_PORT);
-            proxyClient = new Jedis(proxyHost, VIP_SERVICE_PORT);
-            proxyClient.connect();
-        }
-        return proxyClient;
     }
 
 }
